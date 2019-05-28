319e61404458f1afd9cf81a8d48094f743e49e3f
John Hendrikx <hjohn@xs4all.nl>
2019-05-28 22:30:17 +0200

    Split configuration classes in bare minimum database initialization and rest

--- mediasystem-runner/src/main/java/hs/mediasystem/runner/MediaSystemConfigurer.java
+++ mediasystem-runner/src/main/java/hs/mediasystem/runner/MediaSystemConfigurer.java
@@ -1,10 +1,5 @@
 package hs.mediasystem.runner;
 
-import hs.database.core.ConnectionPool;
-import hs.database.core.SimpleConnectionPoolDataSource;
-import hs.database.core.SimpleDatabaseStatementTranslator;
-import hs.database.schema.DatabaseStatementTranslator;
-import hs.database.schema.DatabaseUpdater;
 import hs.ddif.core.Injector;
 import hs.ddif.core.JustInTimeDiscoveryPolicy;
 import hs.ddif.plugins.Plugin;
@@ -16,7 +11,6 @@ import hs.mediasystem.domain.PlayerFactory;
 import hs.mediasystem.runner.expose.Annotations;
 import hs.mediasystem.runner.util.DatabaseResponseCache;
 import hs.mediasystem.util.ini.Ini;
-import hs.mediasystem.util.ini.Section;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -29,16 +23,10 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Properties;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-
-import javax.inject.Provider;
-import javax.sql.ConnectionPoolDataSource;
 
 public class MediaSystemConfigurer {
   private static final Logger LOGGER = Logger.getLogger(MediaSystemConfigurer.class.getName());
@@ -54,7 +42,8 @@ public class MediaSystemConfigurer {
 
     injector.registerInstance(ini);
 
-    configureDatabase(injector, ini.getSection("database"));
+    DatabaseConfigurer.configureDatabase(injector, ini.getSection("database"));
+
     configureResponseCache(injector);
 
     injector.register(DatabaseStreamPrintProvider.class);
@@ -133,85 +122,5 @@ public class MediaSystemConfigurer {
 
   private static Ini createIni() {
     return new Ini(new File("mediasystem.ini"));
-  }
-
-  private static void configureDatabase(Injector injector, Section databaseIniSection) {
-    if(databaseIniSection == null) {
-      try {
-        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-      }
-      catch(ClassNotFoundException e) {
-        throw new IllegalStateException(e);
-      }
-    }
-
-    ConnectionPoolDataSource dataSource = databaseIniSection == null ? new SimpleConnectionPoolDataSource("jdbc:derby:db;create=true") : configureDataSource(databaseIniSection);
-    String databaseUrl = databaseIniSection == null ? "jdbc:derby:db;create=true" : databaseIniSection.get("url");
-
-    LOGGER.info("Using database URL: " + databaseUrl);
-
-    DatabaseStatementTranslator translator = createDatabaseStatementTranslator(databaseUrl);
-
-    injector.register(new Provider<Connection>() {
-      private final ConnectionPool pool = new ConnectionPool(dataSource, 5);
-
-      @Override
-      public Connection get() {
-        return pool.getConnection();
-      }
-    });
-
-    injector.register(new Provider<DatabaseStatementTranslator>() {
-      @Override
-      public DatabaseStatementTranslator get() {
-        return translator;
-      }
-    });
-
-    DatabaseUpdater updater = injector.getInstance(DatabaseUpdater.class);
-
-    updater.updateDatabase("db-scripts");
-  }
-
-  private static DatabaseStatementTranslator createDatabaseStatementTranslator(String url) {
-    String databaseName = url.split(":")[1].toLowerCase();
-
-    if(databaseName.equals("postgresql")) {
-      return new SimpleDatabaseStatementTranslator(new HashMap<String, String>() {{
-        put("BinaryType", "bytea");
-        put("DropNotNull", "DROP NOT NULL");
-        put("Sha256Type", "bytea");
-        put("SerialType", "serial4");
-      }});
-    }
-
-    return new SimpleDatabaseStatementTranslator(new HashMap<String, String>() {{
-      put("BinaryType", "blob");
-      put("DropNotNull", "NULL");
-      put("Sha256Type", "char(32) for bit data");
-      put("SerialType", "integer generated always as identity");
-    }});
-  }
-
-  private static ConnectionPoolDataSource configureDataSource(Section section) {
-    try {
-      Class.forName(section.get("driverClass"));
-      Properties properties = new Properties();
-
-      for(String key : section) {
-        if(!key.equals("driverClass") && !key.equals("postConnectSql") && !key.equals("url")) {
-          properties.put(key, section.get(key));
-        }
-      }
-
-      SimpleConnectionPoolDataSource dataSource = new SimpleConnectionPoolDataSource(section.get("url"), properties);
-
-      dataSource.setPostConnectSql(section.get("postConnectSql"));
-
-      return dataSource;
-    }
-    catch(ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
