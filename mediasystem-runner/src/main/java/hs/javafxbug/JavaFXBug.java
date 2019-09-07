24cdcfd7d94d00e36f548f7b54baebc4ebeb8e30
John Hendrikx <hjohn@xs4all.nl>
2019-09-07 07:26:28 +0200

    Update test program for JavaFX jitter bug

--- mediasystem-runner/src/main/java/hs/javafxbug/JavaFXBug.java
+++ mediasystem-runner/src/main/java/hs/javafxbug/JavaFXBug.java
@@ -2,13 +2,17 @@ package hs.javafxbug;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.geometry.Bounds;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -33,8 +37,6 @@ public class JavaFXBug extends Application {
   public void start(Stage primaryStage) throws Exception {
     MediaStatusIndicatorPane indicator = new MediaStatusIndicatorPane();
 
-    indicator.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
     primaryStage.setScene(new Scene(indicator));
     primaryStage.show();
 
@@ -46,6 +48,29 @@ public class JavaFXBug extends Application {
     tl.setAutoReverse(true);
     tl.setCycleCount(Timeline.INDEFINITE);
     tl.play();
+
+    // Background thread which frequently checks bounds of the arc group,
+    // and stops the animation if the bounds are wrong.  Bounds height
+    // should never exceed 50 (but it often does so slightly).  Sometimes
+    // however it exceeds it by multiple pixels, causing jumps...
+    //
+    // The Width of the bounds also has this problem.
+    //
+    Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(
+      () -> Platform.runLater(() -> {
+        Bounds boundsInLocal = indicator.group.getBoundsInLocal();
+
+        if(boundsInLocal.getHeight() > 54) {
+          System.out.println(boundsInLocal);
+          System.out.println("arc: " + indicator.arc);
+          System.out.println("arc: " + indicator.arc.getBoundsInLocal());
+          tl.stop();
+        }
+      }),
+      0,
+      37,
+      TimeUnit.MILLISECONDS
+    );
   }
 
 }
@@ -53,36 +78,37 @@ public class JavaFXBug extends Application {
 class MediaStatusIndicatorPane extends StackPane {
   public final DoubleProperty value = new SimpleDoubleProperty(0.0);
 
+  public Group group;
+  public Arc arc;
+
   public MediaStatusIndicatorPane() {
     value.addListener(obs -> {
       double percentage = value.get();
 
       List<Node> children = new ArrayList<>();
+      Label bg = new Label();
 
-      children.add(new Label() {{ getStyleClass().add("indicator-pane-background"); }} );
+      bg.setStyle("-fx-background-color: rgba(0,0,0,0.6); -fx-scale-x: 1.1; -fx-scale-y: 1.1; -fx-min-width: 50; -fx-min-height: 50;");
 
-      Arc circle = new Arc(0, 0, 22, 22, 90, 360 * (1 - percentage));
+      children.add(bg);
 
-      circle.getStyleClass().add("indicator-circle-shape");
+      arc = new Arc(0, 0, 22, 22, 90, -360 * percentage);
 
-      Arc arc = new Arc(0, 0, 22, 22, 90, -360 * percentage);
-
+      arc.setStyle("-fx-stroke-width: 5; -fx-fill: transparent; -fx-stroke: rgba(125, 255, 125, 0.5)");
       arc.setType(ArcType.OPEN);
-      arc.getStyleClass().add("indicator-arc-shape");
 
       Rectangle rectangle = new Rectangle(-25, -25, 50, 50);
 
-      rectangle.setFill(Color.TRANSPARENT);
+      rectangle.setFill(new Color(1, 0, 0, 0.3));
 
       Label arcGraphic = new Label();
 
-      arcGraphic.getStyleClass().add("indicator-arc");
-      arcGraphic.setGraphic(new Group(rectangle, arc, circle));
+      group = new Group(rectangle, arc);
+      arcGraphic.setGraphic(group);
       arcGraphic.setAlignment(Pos.TOP_CENTER);
       arcGraphic.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
       children.add(arcGraphic);
-      children.add(new Label(String.format("%d%%", (int)(percentage * 100))) {{ getStyleClass().add("indicator-percentage"); }});
 
       getChildren().setAll(children);
     });
