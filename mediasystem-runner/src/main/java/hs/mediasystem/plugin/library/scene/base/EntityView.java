6ef83da66184638b93a8eac239dea7cb0c45478c
John Hendrikx <hjohn@xs4all.nl>
2019-09-28 00:13:00 +0200

    Fade foreground after 30 seconds of inactivity

--- mediasystem-runner/src/main/java/hs/mediasystem/plugin/library/scene/base/EntityView.java
+++ mediasystem-runner/src/main/java/hs/mediasystem/plugin/library/scene/base/EntityView.java
@@ -3,12 +3,17 @@ package hs.mediasystem.plugin.library.scene.base;
 import hs.mediasystem.presentation.ViewPortFactory;
 import hs.mediasystem.util.javafx.control.GridPaneUtil;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 /**
  * The CollectionView provides a standard area with a background and a small bottom border in which
@@ -44,7 +49,27 @@ public class EntityView extends StackPane {
 
     center = viewPortFactory.create(entityPresentation);
 
+    setupForegroundFade();
+
     getChildren().addAll(stage, content, center);
+  }
+
+  private void setupForegroundFade() {
+    Timeline timeline = new Timeline(
+      new KeyFrame(Duration.seconds(0.5), new KeyValue(center.opacityProperty(), 1.0)),
+      new KeyFrame(Duration.seconds(30), new KeyValue(center.opacityProperty(), 1.0)),
+      new KeyFrame(Duration.seconds(37), new KeyValue(center.opacityProperty(), 0.0))
+    );
+
+    timeline.playFromStart();
+
+    this.addEventFilter(KeyEvent.ANY, e -> {
+      if(timeline.getCurrentTime().greaterThan(Duration.seconds(30))) {
+        e.consume();  // consume first key event when fade out started
+      }
+
+      timeline.playFromStart();
+    });
   }
 
   public void setCenter(Node node) {
