7ebbc7f10cdd2519423f98273afb5e130cf97d09
John Hendrikx <hjohn@xs4all.nl>
2020-01-20 22:43:49 +0100

    Add extra info to Home Screen options, using new stylable containers

--- mediasystem-runner/src/main/java/hs/mediasystem/plugin/home/MenuOptionCellFactory.java
+++ mediasystem-runner/src/main/java/hs/mediasystem/plugin/home/MenuOptionCellFactory.java
@@ -2,13 +2,18 @@ package hs.mediasystem.plugin.home;
 
 import hs.mediasystem.util.ImageHandle;
 import hs.mediasystem.util.ImageHandleFactory;
+import hs.mediasystem.util.SizeFormatter;
 import hs.mediasystem.util.javafx.AsyncImageProperty2;
 import hs.mediasystem.util.javafx.control.BiasedImageView;
-import hs.mediasystem.util.javafx.control.Containers;
 import hs.mediasystem.util.javafx.control.Labels;
 import hs.mediasystem.util.javafx.control.Labels.Feature;
 import hs.mediasystem.util.javafx.control.carousel.CarouselListCell;
+import hs.mediasystem.util.javafx.control.csslayout.StylableContainers;
+import hs.mediasystem.util.javafx.control.csslayout.StylableHBox;
 import hs.mediasystem.util.javafx.control.status.StatusIndicator;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -18,7 +23,6 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.util.Callback;
@@ -38,9 +42,13 @@ public class MenuOptionCellFactory implements Callback<ListView<MenuOption>, Lis
       private final AsyncImageProperty2 asyncImageProperty = new AsyncImageProperty2(600, 400);
 
       private final StatusIndicator indicator = new StatusIndicator();
+      private final Label parentTitle = Labels.create("parent-title", Feature.HIDE_IF_EMPTY);
       private final Label title = Labels.create("title");
       private final Label subtitle = Labels.create("subtitle", Feature.HIDE_IF_EMPTY);
-      private final HBox overlay = Containers.hbox("overlay", indicator, Containers.vbox("title-box", title, subtitle));
+      private final Label sequence = Labels.create("sequence", Feature.HIDE_IF_EMPTY);
+      private final Label age = Labels.create("age", Feature.HIDE_IF_EMPTY);
+
+      private final StylableHBox overlay = StylableContainers.hbox("overlay", indicator, parentTitle, title, subtitle, sequence, age);
 
       private Timeline activeTimeline;
 
@@ -81,8 +89,16 @@ public class MenuOptionCellFactory implements Callback<ListView<MenuOption>, Lis
           asyncImageProperty.imageHandleProperty().set(imageHandle);
           setGraphic(imageView);
 
+          parentTitle.setText(item.getParentTitle());
           title.setText(item.getTitle());
           subtitle.setText(item.getSubtitle());
+          sequence.setText(item.getSequence());
+          age.setText(item.getRecommendationLastTimeWatched()
+            .map(i -> i.atZone(ZoneId.systemDefault()))
+            .map(ZonedDateTime::toLocalDateTime)
+            .map(SizeFormatter::formatTimeAgo)
+            .orElse(null)
+          );
 
           if(item.getWatchedFraction() > 0) {
             indicator.value.setValue(item.getWatchedFraction());
