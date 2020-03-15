fb858601e7081b6a8bdf1674c40b9d06480ceafb
John Hendrikx <hjohn@xs4all.nl>
2020-03-15 15:08:48 +0100

    Fix regression which caused service plugins to not be loaded

--- mediasystem-db/src/main/java/hs/mediasystem/db/ServiceRunner.java
+++ mediasystem-db/src/main/java/hs/mediasystem/db/ServiceRunner.java
@@ -1,16 +1,19 @@
 package hs.mediasystem.db;
 
 import hs.ddif.core.Injector;
+import hs.ddif.core.inject.instantiator.BeanResolutionException;
 import hs.ddif.core.inject.store.BeanDefinitionStore;
 import hs.ddif.plugins.PluginManager;
 
 public class ServiceRunner {
 
-  public static void start(Injector injector) {
+  public static void start(Injector injector) throws BeanResolutionException {
     BeanDefinitionStore store = injector.getStore();
 
     PluginManager pluginManager = new PluginManager(store);
 
     pluginManager.loadPluginAndScan("hs.mediasystem.db", "hs.mediasystem.util", "hs.mediasystem.mediamanager");
+
+    injector.getInstance(PluginInitializer.class);  // Triggers plugin initialization
   }
 }
