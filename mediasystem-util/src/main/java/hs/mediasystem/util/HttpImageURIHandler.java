d84a85372bfc0975616015a9f91f7cd6c35d8332
John Hendrikx <hjohn@xs4all.nl>
2020-03-30 16:07:20 +0200

    Use separate file based image handle for better performance

--- mediasystem-util/src/main/java/hs/mediasystem/util/HttpImageURIHandler.java
+++ mediasystem-util/src/main/java/hs/mediasystem/util/HttpImageURIHandler.java
@@ -2,22 +2,76 @@ package hs.mediasystem.util;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Objects;
-import java.util.logging.Logger;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class HttpImageURIHandler implements ImageURIHandler {
-  private static final Logger LOGGER = Logger.getLogger(HttpImageURIHandler.class.getName());
 
   @Override
   public ImageHandle handle(ImageURI uri) {
-    if(uri.getUri().startsWith("http:") || uri.getUri().startsWith("https:") || uri.getUri().startsWith("file:")) {
+    if(uri.getUri().startsWith("http:") || uri.getUri().startsWith("https:")) {
       return new HttpImageHandle(uri.toURI());
+    }
+    else if(uri.getUri().startsWith("file:")) {
+      return new FileImageHandle(uri.toURI());
     }
 
     return null;
+  }
+
+  private class FileImageHandle implements ImageHandle {
+    private final URI uri;
+
+    public FileImageHandle(URI uri) {
+      this.uri = uri;
+    }
+
+    @Override
+    public byte[] getImageData() throws IOException {
+      return Files.readAllBytes(Paths.get(uri));
+    }
+
+    @Override
+    public String getKey() {
+      return uri.toString();
+    }
+
+    @Override
+    public boolean isFastSource() {
+      return true;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(uri);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if(this == obj) {
+        return true;
+      }
+      if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      }
+
+      FileImageHandle other = (FileImageHandle)obj;
+
+      if(!uri.equals(other.uri)) {
+        return false;
+      }
+
+      return true;
+    }
+
+    @Override
+    public String toString() {
+      return "FileImageHandle[" + uri + "]";
+    }
   }
 
   private class HttpImageHandle implements ImageHandle {
