e241442a0b5130f330687506f4cae23f795fb533
John Hendrikx <hjohn@xs4all.nl>
2020-04-03 19:02:37 +0200

    Performance optimization for SettingsStore

--- mediasystem-db/src/main/java/hs/mediasystem/db/base/SettingsStore.java
+++ mediasystem-db/src/main/java/hs/mediasystem/db/base/SettingsStore.java
@@ -4,13 +4,15 @@ import hs.database.core.Database;
 import hs.database.core.Database.Transaction;
 import hs.database.core.DatabaseException;
 import hs.mediasystem.db.base.Setting.PersistLevel;
+import hs.mediasystem.util.NamedThreadFactory;
 import hs.mediasystem.util.Throwables;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
@@ -20,6 +22,7 @@ import javax.inject.Singleton;
 
 @Singleton
 public class SettingsStore {
+  private static final ScheduledExecutorService EXECUTOR = Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory("SettingStorePersister", true));
   private static final String SEPARATOR = "/::/";
   private static final Logger LOGGER = Logger.getLogger(SettingsStore.class.getName());
   private static final Pattern INT_PATTERN = Pattern.compile("-?\\d+");
@@ -27,62 +30,56 @@ public class SettingsStore {
   @Inject private Database database;
 
   private final Map<String, String> cache = new HashMap<>();
-  private final Set<String> dirtyKeys = new HashSet<>();
+  private final Map<String, String> dirtyEntries = new HashMap<>();
 
   @PostConstruct
   private void postConstruct() {
-    Thread thread = new Thread(() -> {
-      try {
-        for(;;) {
-          Thread.sleep(10000);
+    EXECUTOR.scheduleWithFixedDelay(this::storeDirtyKeys, 10, 10, TimeUnit.SECONDS);
+  }
 
-          Set<String> copiedDirtyKeys;
+  private void storeDirtyKeys() {
+    Map<String, String> copiedDirtyEntries;
 
-          synchronized(dirtyKeys) {
-            if(dirtyKeys.isEmpty()) {
-              continue;
-            }
+    synchronized(dirtyEntries) {
+      if(dirtyEntries.isEmpty()) {
+        return;
+      }
 
-            copiedDirtyKeys = new HashSet<>(dirtyKeys);
-            dirtyKeys.clear();
-          }
+      copiedDirtyEntries = new HashMap<>(dirtyEntries);
+      dirtyEntries.clear();
+    }
 
-          try(Transaction tx = database.beginTransaction()) {
-            for(String key : copiedDirtyKeys) {
-              int index = key.indexOf(SEPARATOR);
-              String system = key.substring(0, index);
-              String name = key.substring(index + SEPARATOR.length());
-              Setting setting = tx.selectUnique(Setting.class, "system = ? AND name = ?", system, name);
+    try(Transaction tx = database.beginTransaction()) {
+      Date now = new Date();
 
-              if(setting == null) {
-                setting = new Setting();
+      for(Map.Entry<String, String> entry : copiedDirtyEntries.entrySet()) {
+        String key = entry.getKey();
+        int index = key.indexOf(SEPARATOR);
+        String system = key.substring(0, index);
+        String name = key.substring(index + SEPARATOR.length());
+        Setting setting = tx.selectUnique(Setting.class, "system = ? AND name = ?", system, name);
 
-                setting.setSystem(system);
-                setting.setKey(name);
-                setting.setPersistLevel(PersistLevel.PERMANENT);
-              }
+        if(setting == null) {
+          setting = new Setting();
 
-              setting.setValue(cache.get(key));
-              setting.setLastUpdated(new Date());
-
-              tx.merge(setting);
-            }
-
-            tx.commit();
-          }
-          catch(DatabaseException e) {
-            LOGGER.warning("Unable to store modified settings: " + Throwables.formatAsOneLine(e));
-          }
+          setting.setSystem(system);
+          setting.setKey(name);
+          setting.setPersistLevel(PersistLevel.PERMANENT);
         }
-      }
-      catch(InterruptedException e) {
-        throw new IllegalStateException(e);
-      }
-    });
 
-    thread.setName("SettingStorePersister");
-    thread.setDaemon(true);
-    thread.start();
+        setting.setValue(entry.getValue());
+        setting.setLastUpdated(now);
+
+        tx.merge(setting);
+      }
+
+      tx.commit();
+
+      LOGGER.fine("Stored settings: " + copiedDirtyEntries);
+    }
+    catch(DatabaseException e) {
+      LOGGER.warning("Unable to store modified settings: " + Throwables.formatAsOneLine(e));
+    }
   }
 
   public String getSetting(String system, String name) {
@@ -126,12 +123,10 @@ public class SettingsStore {
   public void storeSetting(String system, String name, String value) {
     String key = key(system, name);
 
-    LOGGER.fine("Stored value for '" + key + "': " + value);
-
     cache.put(key, value);
 
-    synchronized(dirtyKeys) {
-      dirtyKeys.add(key);
+    synchronized(dirtyEntries) {
+      dirtyEntries.put(key, value);
     }
   }
 
