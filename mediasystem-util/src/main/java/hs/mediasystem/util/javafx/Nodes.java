cc6cc6165cc893b836164868e074fa8092fbe8db
John Hendrikx <hjohn@xs4all.nl>
2020-09-11 06:27:02 +0200

    Add safe selection bind code as a utility

--- mediasystem-util/src/main/java/hs/mediasystem/util/javafx/Nodes.java
+++ mediasystem-util/src/main/java/hs/mediasystem/util/javafx/Nodes.java
@@ -2,9 +2,14 @@ package hs.mediasystem.util.javafx;
 
 import java.util.Optional;
 
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionModel;
 import javafx.stage.Window;
 
 import org.reactfx.value.Val;
@@ -22,6 +27,53 @@ public class Nodes {
 
   public static boolean isTreeVisible(Node node) {
     return node.isVisible() && (node.getParent() == null || isTreeVisible(node.getParent()));
+  }
+
+  public static <T> void listenWhenNodeVisible(Node node, ObservableValue<T> property, ChangeListener<? super T> listener) {
+    visible(node).addListener((obs, old, visible) -> {
+      if(visible) {
+        property.addListener(listener);
+      }
+      else {
+        property.removeListener(listener);
+      }
+    });
+  }
+
+  public static void bindVisibilityEvents(Node node, Runnable whenShown, Runnable whenHidden) {
+    visible(node).addListener((obs, old, visible) -> {
+      if(visible) {
+        whenShown.run();
+      }
+      else {
+        whenHidden.run();
+      }
+    });
+  }
+
+  public static <T> void safeBindBidirectionalSelectedItemToModel(ListView<T> view, Property<T> modelProperty) {
+    SelectionModel<T> selectionModel = view.getSelectionModel();
+
+    ChangeListener<T> updatePresentation = (obs, old, current) -> modelProperty.setValue(current);
+
+    ChangeListener<T> updateView = (obs, old, current) -> {
+      if(!selectionModel.getSelectedItem().equals(current)) {
+        selectionModel.select(current);
+      }
+    };
+
+    Nodes.bindVisibilityEvents(
+      view,
+      () -> {  // when shown
+        selectionModel.select(modelProperty.getValue());
+        selectionModel.selectedItemProperty().addListener(updatePresentation);
+        modelProperty.addListener(updateView);
+      },
+      () -> {  // when hidden
+        selectionModel.selectedItemProperty().removeListener(updatePresentation);
+        modelProperty.removeListener(updateView);
+      }
+    );
   }
 
   /**
