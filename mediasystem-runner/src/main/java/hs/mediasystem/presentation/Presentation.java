547b25aa3bcd9b9d915b75c5f803f0665824aa0b
John Hendrikx <hjohn@xs4all.nl>
2021-01-02 13:14:05 +0100

    Clarify docs

--- mediasystem-runner/src/main/java/hs/mediasystem/presentation/Presentation.java
+++ mediasystem-runner/src/main/java/hs/mediasystem/presentation/Presentation.java
@@ -5,9 +5,9 @@ public interface Presentation {
   /**
    * Fetches the most recent data for this presentation and stores the
    * result in an update task.  Running the resulting task will update
-   * the presentation with the refreshed data.  Note that this task must
-   * be run on the UI thread if the Presentation is currently attached to
-   * a UI.<p>
+   * the presentation with the refreshed data.  Note that the resulting
+   * task must be run on the UI thread if the Presentation is currently
+   * attached to a UI.<p>
    *
    * This is a two step operation, where the creation of the update task
    * can be expensive and should be run in the background.  The resulting
