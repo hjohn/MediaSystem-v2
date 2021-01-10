e5e82cf45db6224d65d44ecddb226fbc51dada58
John Hendrikx <hjohn@xs4all.nl>
2021-01-10 16:05:12 +0100

    Add grouping by Genre to TV shows

--- mediasystem-runner/src/main/java/hs/mediasystem/plugin/series/menu/SeriesCollectionType.java
+++ mediasystem-runner/src/main/java/hs/mediasystem/plugin/series/menu/SeriesCollectionType.java
@@ -1,19 +1,24 @@
 package hs.mediasystem.plugin.series.menu;
 
 import hs.mediasystem.domain.stream.MediaType;
-import hs.mediasystem.plugin.library.scene.WorkBinder;
 import hs.mediasystem.plugin.library.scene.grid.GenericCollectionPresentationFactory;
 import hs.mediasystem.plugin.library.scene.grid.GridViewPresentationFactory.Filter;
 import hs.mediasystem.plugin.library.scene.grid.GridViewPresentationFactory.SortOrder;
 import hs.mediasystem.plugin.library.scene.grid.GridViewPresentationFactory.ViewOptions;
 import hs.mediasystem.presentation.Presentation;
 import hs.mediasystem.runner.collection.CollectionType;
+import hs.mediasystem.runner.grouping.GenreGrouping;
+import hs.mediasystem.runner.grouping.NoGrouping;
+import hs.mediasystem.runner.grouping.WorksGroup;
 import hs.mediasystem.ui.api.WorksClient;
+import hs.mediasystem.ui.api.domain.Details;
+import hs.mediasystem.ui.api.domain.State;
 import hs.mediasystem.ui.api.domain.Work;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -21,25 +26,29 @@ import javax.inject.Singleton;
 
 @Singleton
 public class SeriesCollectionType implements CollectionType {
+  private static final Comparator<Object> BY_NAME = Comparator.comparing(SeriesCollectionType::extractDetails, Details.ALPHABETICAL);
+  private static final Comparator<Object> BY_RELEASE_DATE_REVERSED = Comparator.comparing(SeriesCollectionType::extractDetails, Details.RELEASE_DATE_REVERSED);
+  private static final Comparator<Object> BY_LAST_WATCHED_REVERSED = Comparator.comparing(SeriesCollectionType::extractState, State.WATCHED_DATE_REVERSED);
 
-  private static final List<SortOrder<Work>> SORT_ORDERS = List.of(
-    new SortOrder<>("alpha", WorkBinder.BY_NAME),
-    new SortOrder<>("release-date", WorkBinder.BY_RELEASE_DATE.reversed()),
-    new SortOrder<>("watched-date", WorkBinder.BY_LAST_WATCHED_DATE.reversed())
+  private static final List<SortOrder<Object>> SORT_ORDERS = List.of(
+    new SortOrder<>("alpha", BY_NAME),
+    new SortOrder<>("release-date", BY_RELEASE_DATE_REVERSED),
+    new SortOrder<>("watched-date", BY_LAST_WATCHED_REVERSED)
   );
 
-  private static final List<Filter<Work>> FILTERS = List.of(
-    new Filter<>("none", r -> true),
-    new Filter<>("released-recently", r -> r.getDetails().getReleaseDate().filter(d -> d.isAfter(LocalDate.now().minusYears(5))).isPresent()),
-    new Filter<>("watched-recently", r -> r.getState().getLastConsumptionTime().filter(d -> d.isAfter(Instant.now().minus(365 * 2, ChronoUnit.DAYS))).isPresent())
+  private static final List<Filter<Object>> FILTERS = List.of(
+    new Filter<>("none", w -> true),
+    new Filter<>("released-recently", w -> extractDetails(w).getReleaseDate().filter(d -> d.isAfter(LocalDate.now().minusYears(5))).isPresent()),
+    new Filter<>("watched-recently", w -> extractState(w).getLastConsumptionTime().filter(d -> d.isAfter(Instant.now().minus(365 * 2, ChronoUnit.DAYS))).isPresent())
   );
 
-  private static final List<Filter<Work>> STATE_FILTERS = List.of(
-    new Filter<>("none", r -> true),
-    new Filter<>("unwatched", r -> !r.getState().isConsumed())
+  private static final List<Filter<Object>> STATE_FILTERS = List.of(
+    new Filter<>("none", w -> true),
+    new Filter<>("unwatched", w -> !extractState(w).isConsumed())
   );
 
   @Inject private GenericCollectionPresentationFactory factory;
+  @Inject private GenreGrouping genreGrouper;
   @Inject private WorksClient worksClient;
 
   @Override
@@ -52,8 +61,35 @@ public class SeriesCollectionType implements CollectionType {
     return factory.create(
       () -> worksClient.findAllByType(MediaType.SERIE, tag),
       "Series" + (tag == null ? "" : ":" + tag),
-      new ViewOptions<>(SORT_ORDERS, FILTERS, STATE_FILTERS),
+      new ViewOptions<>(
+        SORT_ORDERS,
+        FILTERS,
+        STATE_FILTERS,
+        List.of(genreGrouper, new NoGrouping<Work, Object>())
+      ),
       null
     );
+  }
+
+  private static Details extractDetails(Object obj) {
+    if(obj instanceof Work) {
+      Work work = (Work)obj;
+
+      return work.getDetails();
+    }
+
+    WorksGroup wg = (WorksGroup)obj;
+
+    return wg.getDetails();
+  }
+
+  private static State extractState(Object obj) {
+    if(obj instanceof Work) {
+      Work work = (Work)obj;
+
+      return work.getState();
+    }
+
+    return State.EMPTY;
   }
 }
--- mediasystem-ui-api/src/main/java/hs/mediasystem/ui/api/domain/State.java
+++ mediasystem-ui-api/src/main/java/hs/mediasystem/ui/api/domain/State.java
@@ -2,9 +2,11 @@ package hs.mediasystem.ui.api.domain;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.Optional;
 
 public class State {
+  public static final Comparator<State> WATCHED_DATE_REVERSED = Comparator.comparing((State s) -> s.getLastConsumptionTime().orElse(null), Comparator.nullsLast(Comparator.naturalOrder())).reversed();
   public static final State EMPTY = new State(null, false, Duration.ZERO);
 
   private final Optional<Instant> lastConsumptionTime;
