4f30d025269cd5f9e6a1e4ab1d592e42e024682c
John Hendrikx <hjohn@xs4all.nl>
2021-03-29 08:36:29 +0200

    Add FILL_WIDTH and FILL_HEIGHT options for creating hbox/vbox containers

--- mediasystem-util/src/main/java/hs/mediasystem/util/javafx/control/Containers.java
+++ mediasystem-util/src/main/java/hs/mediasystem/util/javafx/control/Containers.java
@@ -25,34 +25,37 @@ import javafx.scene.layout.VBox;
  * Multiple style classes can be assigned by providing a comma separated string.
  */
 public class Containers {
-  public static final Option HIDE_IF_EMPTY = pane -> hide(Bindings.isEmpty(pane.getChildren().filtered(Node::isVisible)));
-  public static final Option MOUSE_TRANSPARENT = pane -> pane.setMouseTransparent(true);
+  public static final Option<Pane> HIDE_IF_EMPTY = pane -> hide(Bindings.isEmpty(pane.getChildren().filtered(Node::isVisible)));
+  public static final Option<Pane> MOUSE_TRANSPARENT = pane -> pane.setMouseTransparent(true);
+  public static final Option<VBox> FILL_WIDTH = pane -> pane.setFillWidth(true);
+  public static final Option<HBox> FILL_HEIGHT = pane -> pane.setFillHeight(true);
 
-  public static final Option hideIfEmpty(StringExpression x) {
+  public static final Option<Pane> hideIfEmpty(StringExpression x) {
     return hide(x.isEmpty());
   }
 
-  public static final Option hide(BooleanExpression x) {
+  public static final Option<Pane> hide(BooleanExpression x) {
     return pane -> {
       pane.managedProperty().bind(x.not());
       pane.visibleProperty().bind(x.not());
     };
   }
 
-  public static final Option apply(Consumer<Pane> paneConsumer) {
+  public static final Option<Pane> apply(Consumer<Pane> paneConsumer) {
     return pane -> paneConsumer.accept(pane);
   }
 
-  public interface Option extends Consumer<Pane> {
+  public interface Option<T extends Pane> extends Consumer<T> {
   }
 
-  public static HBox hbox(String styleClass, List<Node> nodes, Option... options) {
+  @SafeVarargs
+  public static HBox hbox(String styleClass, List<Node> nodes, Option<? super HBox>... options) {
     HBox hbox = new HBox();
 
     addChildren(hbox, nodes);
     addStyleClass(hbox, styleClass);
 
-    for(Option option : options) {
+    for(Option<? super HBox> option : options) {
       option.accept(hbox);
     }
 
@@ -67,7 +70,8 @@ public class Containers {
     return hbox(null, nodes);
   }
 
-  public static VBox vbox(String styleClass, BooleanBinding visibility, List<Node> nodes, Option... options) {
+  @SafeVarargs
+  public static VBox vbox(String styleClass, BooleanBinding visibility, List<Node> nodes, Option<? super VBox>... options) {
     VBox vbox = new VBox();
 
     addChildren(vbox, nodes);
@@ -78,7 +82,7 @@ public class Containers {
       vbox.visibleProperty().bind(visibility);
     }
 
-    for(Option option : options) {
+    for(Option<? super VBox> option : options) {
       option.accept(vbox);
     }
 
@@ -89,7 +93,8 @@ public class Containers {
     return vbox(styleClass, visibility, Arrays.asList(nodes));
   }
 
-  public static VBox vbox(String styleClass, List<Node> nodes, Option... options) {
+  @SafeVarargs
+  public static VBox vbox(String styleClass, List<Node> nodes, Option<? super VBox>... options) {
     return vbox(styleClass, null, nodes, options);
   }
 
@@ -105,6 +110,15 @@ public class Containers {
     return vbox(null, null, nodes);
   }
 
+  @SafeVarargs
+  public static VBox vbox(List<Node> nodes, Option<? super VBox>... options) {
+    return vbox(null, null, nodes, options);
+  }
+
+  public static VBox vbox(List<Node> nodes) {
+    return vbox(null, null, nodes);
+  }
+
   public static GridPane grid(String styleClass) {
     return addStyleClass(new GridPane(), styleClass);
   }
@@ -117,12 +131,13 @@ public class Containers {
     return stack(styleClass, Arrays.asList(nodes));
   }
 
-  public static StackPane stack(String styleClass, List<Node> nodes, Option... options) {
+  @SafeVarargs
+  public static StackPane stack(String styleClass, List<Node> nodes, Option<? super StackPane>... options) {
     StackPane stackPane = new StackPane();
 
     addChildren(stackPane, nodes);
 
-    for(Option option : options) {
+    for(Option<? super StackPane> option : options) {
       option.accept(stackPane);
     }
 
