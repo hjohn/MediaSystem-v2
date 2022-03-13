0f97b25867bac0fba6fab80baf0f5d3c19757b62
John Hendrikx <hjohn@xs4all.nl>
2022-03-13 09:42:50 +0100

    Use correct years and fall back to latest information if group misses it

--- mediasystem-runner/src/main/java/hs/mediasystem/runner/grouping/CollectionGrouping.java
+++ mediasystem-runner/src/main/java/hs/mediasystem/runner/grouping/CollectionGrouping.java
@@ -3,9 +3,12 @@ package hs.mediasystem.runner.grouping;
 import hs.mediasystem.domain.stream.MediaType;
 import hs.mediasystem.domain.work.WorkId;
 import hs.mediasystem.ui.api.WorkClient;
+import hs.mediasystem.ui.api.domain.Details;
 import hs.mediasystem.ui.api.domain.Work;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +22,7 @@ public class CollectionGrouping implements Grouping<Work, Object> {
 
   @Override
   public List<Object> group(List<? extends Work> items) {
+    LocalDate now = LocalDate.now().plusDays(1);
     Map<WorkId, List<Work>> childWorks = new HashMap<>();
     List<Object> topLevelItems = new ArrayList<>();
 
@@ -28,7 +32,7 @@ public class CollectionGrouping implements Grouping<Work, Object> {
           workClient.find(p.getId()).ifPresent(r -> {
             childWorks.put(p.getId(), workClient.findChildren(p.getId()));
 
-            topLevelItems.add(createWorkGroup(r, childWorks.get(p.getId())));
+            topLevelItems.add(createWorkGroup(r, childWorks.get(p.getId()), now));
           });
         }
       });
@@ -41,7 +45,33 @@ public class CollectionGrouping implements Grouping<Work, Object> {
     return topLevelItems;
   }
 
-  private static WorksGroup createWorkGroup(Work work, List<Work> list) {
-    return new WorksGroup(work.getId(), work.getDetails(), list);
+  private static WorksGroup createWorkGroup(Work work, List<Work> list, LocalDate now) {
+    Work latest = list.stream()
+      .sorted(Comparator.comparing(Work::getDetails, Details.RELEASE_DATE_REVERSED))
+      .filter(c -> now.isAfter(c.getDetails().getReleaseDate().orElse(LocalDate.MAX)))
+      .findFirst()
+      .orElse(work);
+
+    Details latestDetails = latest.getDetails();
+    Details groupDetails = work.getDetails();
+
+    Details combinedDetails = new Details(
+      groupDetails.getTitle(),
+      groupDetails.getSubtitle().orElse(null),
+      groupDetails.getDescription().orElse(null),
+      latestDetails.getReleaseDate().orElse(null),
+      groupDetails.getCover().or(latestDetails::getCover).orElse(null),
+      groupDetails.getAutoCover().or(latestDetails::getAutoCover).orElse(null),
+      groupDetails.getSampleImage().or(latestDetails::getSampleImage).orElse(null),
+      groupDetails.getBackdrop().or(latestDetails::getBackdrop).orElse(null),
+      groupDetails.getTagline().orElse(null),
+      groupDetails.getSerie().orElse(null),
+      groupDetails.getSequence().orElse(null),
+      groupDetails.getReception().orElse(null),
+      groupDetails.getPopularity().orElse(null),
+      groupDetails.getClassification()
+    );
+
+    return new WorksGroup(work.getId(), combinedDetails, list);
   }
 }
