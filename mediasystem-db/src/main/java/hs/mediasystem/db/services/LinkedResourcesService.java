617c59ecff8813ac86e2e7ba650fd02a3b35a520
John Hendrikx <hjohn@xs4all.nl>
2022-09-19 07:56:35 +0200

    Store LinkedResources nested by ContentID to improve performance

--- mediasystem-db/src/main/java/hs/mediasystem/db/services/LinkedResourcesService.java
+++ mediasystem-db/src/main/java/hs/mediasystem/db/services/LinkedResourcesService.java
@@ -38,6 +38,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -55,13 +56,14 @@ public class LinkedResourcesService {
    * not been matched up to a known episode or special.
    */
   private static final String DEFAULT_DATA_SOURCE_NAME = "@INTERNAL";
+  private static final MultiMapper<StreamID, LinkedResource> MAPPER = new MultiMapper<>(LinkedResource::id);
 
   @Inject private DescriptorStore descriptorStore;
   @Inject private EventSource<StreamableEvent> streamableEvents;
   @Inject private ImportSourceProvider importSourceProvider;
   @Inject private ContentPrintProvider contentPrintProvider;
 
-  private final Map<StreamID, LinkedResource> linkedResources = new HashMap<>();
+  private final Map<ContentID, Object> linkedResourcesByContentId = new HashMap<>();
   private final EventStream<LinkedResourceEvent> eventStream = new InMemoryEventStream<>();
 
   @Inject
@@ -85,25 +87,23 @@ public class LinkedResourcesService {
    * @return an optional {@link LinkedResource}, never {@code null}
    */
   public Optional<LinkedResource> findFirst(ContentID cid) {
-    synchronized(linkedResources) {
-      return linkedResources.values().stream()
-        .filter(lr -> lr.id().getContentId().equals(cid))
-        .findFirst();
+    synchronized(linkedResourcesByContentId) {
+      return MAPPER.findFirst(linkedResourcesByContentId.get(cid));
     }
   }
 
   private void processEvent(StreamableEvent event) {
-    synchronized(linkedResources) {
+    synchronized(linkedResourcesByContentId) {
       if(event instanceof StreamableEvent.Updated u) {
         CachedStream cs = u.cachedStream();
         LinkedResource resource = toStream(cs);
 
-        linkedResources.put(cs.getStreamable().getId(), resource);
+        linkedResourcesByContentId.compute(cs.getStreamable().getId().getContentId(), (k, v) -> MAPPER.put(v, resource));
 
         eventStream.push(new Event<>(new LinkedResourceEvent.Updated(resource)));
       }
       else if(event instanceof StreamableEvent.Removed r) {
-        linkedResources.remove(r.id());
+        linkedResourcesByContentId.compute(r.id().getContentId(), (k, v) -> MAPPER.remove(v, r.id()));
 
         eventStream.push(new Event<>(new LinkedResourceEvent.Removed(r.id())));
       }
@@ -172,7 +172,7 @@ public class LinkedResourcesService {
   }
 
   private Optional<Parent> createParent(StreamID parentId) {
-    LinkedResource parent = linkedResources.get(parentId);
+    LinkedResource parent = MAPPER.get(linkedResourcesByContentId.get(parentId.getContentId()), parentId);
 
     if(parent == null || parent.works().isEmpty()) {
       return Optional.empty();
@@ -221,5 +221,95 @@ public class LinkedResourcesService {
       null,
       null
     );
+  }
+
+  /**
+   * Helper class that simplifies tracking zero, one or more key value pairs with minimal overhead.
+   * The helper requires the current base value for all its functions. When modified the mapper
+   * returns the new base value, which can be {@code null} if there are no key value pairs, just
+   * a value if there is a single key value pair, or a {@code Map} if there are multiple pairs.
+   *
+   * @param <K> the type of the keys
+   * @param <V> the type of the values
+   */
+  static class MultiMapper<K, V> {
+    private final Function<V, K> keyExtractor;
+
+    public MultiMapper(Function<V, K> keyExtractor) {
+      this.keyExtractor = keyExtractor;
+    }
+
+    /**
+     * Returns the value associated with the given key, or {@code null} if there is none.
+     *
+     * @param base a base to examine, can be {@code null}
+     * @param key a key, can be {@code null}
+     * @return the value associated with the given key, or {@code null} if there is none
+     */
+    public V get(Object base, K key) {
+      if(base == null) {
+        return null;
+      }
+
+      if(base instanceof Map) {
+        @SuppressWarnings("unchecked")
+        Map<K, V> map = (Map<K, V>)base;
+
+        return map.get(key);
+      }
+
+      @SuppressWarnings("unchecked")
+      V baseValue = (V)base;
+      K baseKey = keyExtractor.apply(baseValue);
+
+      return baseKey.equals(key) ? baseValue : null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Optional<V> findFirst(Object base) {
+      return Optional.ofNullable(base == null ? null : base instanceof Map ? ((Map<K, V>)base).values().iterator().next() : (V)base);
+    }
+
+    public Object put(Object base, V value) {
+      if(base == null) {
+        return value;
+      }
+
+      K key = keyExtractor.apply(value);
+
+      if(base instanceof Map) {
+        @SuppressWarnings("unchecked")
+        Map<K, V> map = (Map<K, V>)base;
+
+        map.put(key, value);
+
+        return base;
+      }
+
+      @SuppressWarnings("unchecked")
+      K baseKey = keyExtractor.apply((V)base);
+
+      return key.equals(baseKey) ? value : new HashMap<>(Map.of(baseKey, base, key, value));
+    }
+
+    public Object remove(Object base, K key) {
+      if(base == null) {
+        return null;
+      }
+
+      if(base instanceof Map) {
+        @SuppressWarnings("unchecked")
+        Map<K, V> map = (Map<K, V>)base;
+
+        map.remove(key);
+
+        return map.size() == 1 ? map.values().iterator().next() : map;
+      }
+
+      @SuppressWarnings("unchecked")
+      K baseKey = keyExtractor.apply((V)base);
+
+      return baseKey.equals(key) ? null : base;
+    }
   }
 }
